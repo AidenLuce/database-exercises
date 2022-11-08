dfd36c23519f568b2159f08d9ee16efe992454e1
Aiden Luce <luceaiden4124@gmail.com>
2022-11-08 09:06:13 -0600

    sql exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -9,7 +9,7 @@ SELECT name FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums SET sales = sales *10;
 SELECT * FROM albums;
 
-UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
+UPDATE albums SET release_date = release_date -100 WHERE release_date < 1980;
 SELECT * FROM albums;
 
 UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
